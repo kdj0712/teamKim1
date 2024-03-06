0df0e0e1672aaa17caecf2674fcca88ab84dd01a
DeokJae Kim <kdj0712@gmail.com>
2024-03-06 02:08:20 +0000

    commit

--- models/member.py
+++ models/member.py
@@ -1,19 +1,19 @@
-from typing import Optional, List
+from typing import Optional, List, Union
 
 from beanie import Document, Link
 from pydantic import BaseModel, EmailStr
 
 class members(Document):
-    user_ID: Optional[str] = None
-    user_pswd: Optional[str] = None
+    user_ID: Optional[Union[str, int, float, bool]] = None
+    user_pswd: Optional[Union[str, int, float, bool]] = None
     user_email: Optional[EmailStr] = None    
-    user_name: Optional[str] = None
-    user_phone : Optional[str] = None
-    user_info : Optional[str] = None
-    user_birth : Optional[str] = None
-    user_postcode : Optional[str] = None
-    user_address : Optional[str] = None
-    user_detailed_address : Optional[str] = None
+    user_name: Optional[Union[str, int, float, bool]] = None
+    user_phone : Optional[Union[str, int, float, bool]] = None
+    user_info : Optional[Union[str, int, float, bool]] = None
+    user_birth : Optional[Union[str, int, float, bool]] = None
+    user_postcode : Optional[Union[str, int, float, bool]] = None
+    user_address : Optional[Union[str, int, float, bool]] = None
+    user_detailed_address : Optional[Union[str, int, float, bool]] = None
     
   
     class Settings:
