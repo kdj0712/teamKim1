8e3bebd875ef9489eb38872406b319b34ff0d034
DeokJae Kim <kdj0712@gmail.com>
2024-03-14 06:10:51 +0000

    commit

--- route/info.py
+++ route/info.py
@@ -1,16 +1,22 @@
-from fastapi import APIRouter, FastAPI, Form, Depends, Query
+from fastapi import APIRouter, FastAPI, Form, Depends, Query, HTTPException
 from typing import Optional
 from starlette.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from aiohttp import ClientSession
 from fastapi import Request
+from pydantic import BaseModel
 from typing import Optional
 from dotenv import load_dotenv
 import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from google.cloud import storage
+# from google.oauth2 import service_account
+import pickle
 load_dotenv()
 import os
 api_key = os.getenv("API_KEY")
-
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/app/teamKim/macro-atom-415806-fd4035d471e1.json"
 router = APIRouter()
 
 from database.connection import Database
@@ -30,6 +36,16 @@ templates = Jinja2Templates(directory="templates/")
 
 #### -------------------------------------------------------------------------------------------------------
 
+def load_pickle_from_gcs(bucket_name, file_name):
+    """Google Cloud Storage에서 피클 파일을 로드합니다."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    pickle_data = blob.download_as_bytes()
+    return pickle.loads(pickle_data)
+bucket_name = 'savehomes'
+tfidf_vectorizer_file_name = 'search_symptoms.pkl'
+vectorizer = load_pickle_from_gcs(bucket_name, tfidf_vectorizer_file_name)
 # 희귀질환정보검색
 @router.post("/info_raredisease", response_class=HTMLResponse) 
 async def raredisease(request:Request):
@@ -56,7 +72,8 @@ async def list(
                 {"dise_name_kr": {'$regex': search_word}},
                 {"dise_name_en": {'$regex': search_word}},
                 {"dise_support": {'$regex': search_word}},
-                {"dise_url": {'$regex': search_word}}
+                {"dise_url": {'$regex': search_word}},
+                {"dise_symptoms": {'$regex': search_word}}
             ]
         })
 
