866d72e76759a6ad33dbde8c938df746978f4497
sky <adsky0309@gmail.com>
2024-03-21 03:51:11 +0000

    d

--- data/selenium/schedular/news_recommend.py
+++ data/selenium/schedular/news_recommend.py
@@ -2,6 +2,7 @@
 from pymongo import MongoClient
 import pickle
 import os
+from konlpy.tag import Okt
 
 def dbconnect(Database_name, collection_name) :
     mongoClient = MongoClient("mongodb://192.168.10.236:27017/")
@@ -9,6 +10,17 @@ def dbconnect(Database_name, collection_name) :
     collection = database[collection_name]
     return collection
 
+def tokenizer(raw, pos=['Noun', 'Verb']):
+    stopword = ['서울대', '희귀질환', '희귀', '대다',  '케다', '소아', '생명', '한국', '한미','사노피', '하다', '급여', '국내', '샤이어',  '스케', '세포'
+            , '병원',  '질환',  '한독', '화이자제약',  '전달', '질병', '인하대병원',  '관리', '다국적', '환자', '지정', '치료'
+            , '오다', '헌터', '작년', '브리', '위해', '베다', '받다', '심평원', '코로나', '건보', '화순', '전남대', '실시', '자임','녹십자'
+            ] #추가 생성 필요
+    okt=Okt()
+    return [
+        word for word, tag in okt.pos(raw, norm=True, stem=True)
+        if len(word) >1 and tag in pos and word not in stopword
+    ]
+    
 
 def update_news_trend_collection():
     news_weekly_collection = dbconnect("Seleniums", "bosa_news_weekly")
@@ -25,14 +37,17 @@ def update_news_trend_collection():
     with open(os.path.join('data', 'pkl', 'news_recommend_model.pkl'), "rb") as file:
         model = pickle.load(file)
 
+    # vectorizer(tokenizer=tokenizer)
+
     for document in cursor :
         news_title = document.get('news_title')
-        news_topic = model.predict(vectorizer.transform([news_title]))
+        news_topic_array = model.predict(vectorizer.transform([news_title]))
+        news_topic = ''.join(news_topic_array)
         document['news_topic'] = news_topic
         news_collection.insert_one(document)
+        pass
 
     # news_trend_collection에 내용 추가하기
-
 
 if __name__ == "__main__" :
     update_news_trend_collection()
--- data/selenium/schedular/schedular.py
+++ data/selenium/schedular/schedular.py
@@ -1,10 +1,13 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.combining import AndTrigger
 
 # 정의한 함수 불러오기
-from schedular.news_scrappnig_weekly import bosascrapping
-from schedular.news_recommend import
+from news_scrappnig_weekly import bosascrapping
+from news_recommend import update_news_trend_collection
 
 if __name__ == "__main__" : 
     schedular = BackgroundScheduler()
-    schedular.add_job(bosascrapping("http://www.bosa.co.kr/", "희귀질환"), trigger="interval", seconds=604800, coalesce=True, max_instances=1) #주기 설정 필요
-    
+    schedular.add_job(bosascrapping,args=["http://www.bosa.co.kr/", "희귀질환"] , trigger="interval", seconds=604800) #주기 설정 필요
+    chained_trigger = AndTrigger(trigger1=bosascrapping, trigger2='date', job_id='update_job')
+    schedular.add_job(update_news_trend_collection, trigger= chained_trigger)
+    schedular.start()
