4f8a0bd326cc8e0718dbce602d2ff6f9ecc96a42
sky <adsky0309@gmail.com>
2024-03-21 12:13:50 +0900

    d

--- data/selenium/schedular/schedular.py
+++ data/selenium/schedular/schedular.py
@@ -1,8 +1,10 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
 # 정의한 함수 불러오기
-from schedular.news_scrappnig_weekly import
+from schedular.news_scrappnig_weekly import bosascrapping
 from schedular.news_recommend import
 
-
-
+if __name__ == "__main__" : 
+    schedular = BackgroundScheduler()
+    schedular.add_job(bosascrapping("http://www.bosa.co.kr/", "희귀질환"), trigger="interval", seconds=604800, coalesce=True, max_instances=1) #주기 설정 필요
+    
